7900084788052a81a66e2a4ad9ff4e5d3be6d017
Felipe Spirandelli <felipereisspirandelli@gmail.com>
2023-09-29 18:27:22 -0300

    feat: setup pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,17 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
+import { Ubuntu } from "next/font/google";
+import Head from "next/head";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+
+const ubuntu = Ubuntu({
+  subsets: ["latin-ext"],
+  weight: ["300", "400", "500", "700"],
+});
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -12,7 +19,13 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <Head>
+        <title>ShapeSync</title>
+        <meta name="description" content="ShapeSync. Melhor lugar para acompanhar o shape" key="title" />
+      </Head>
+      <main className={ubuntu.className}>
+        <Component {...pageProps} />
+      </main>
     </SessionProvider>
   );
 };
