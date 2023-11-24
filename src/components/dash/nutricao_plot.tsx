14bcf54e7c517b16c29b5a56896f781f193fe4f7
alex81632 <alexandrebellargus@hotmail.com>
2023-11-24 17:31:49 -0300

    ajuste dash

--- src/components/dash/nutricao_plot.tsx
+++ src/components/dash/nutricao_plot.tsx
@@ -6,6 +6,7 @@ import { Alimentos } from '@prisma/client';
 const alimentos = alimentosData.alimentos;
 
 const COLORS = ['#0088FE', '#00C49F', '#FFBB28'];
+const COLORS_FADE = ['#0088FE80', '#00C49F80', '#FFBB2880'];
 
 const BarChart1 = (props: { data: Alimentos[] | null }) => {
 
@@ -38,21 +39,32 @@ const BarChart1 = (props: { data: Alimentos[] | null }) => {
   }
 
   const newData = [
-    { name: 'Proteinas', value: proteinas },
-    { name: 'Carboidratos', value: carboidratos },
-    { name: 'Gorduras', value: gorduras },
+    { name: 'Proteinas g', value: proteinas },
+    { name: 'Carboidratos g', value: carboidratos },
+    { name: 'Gorduras g', value: gorduras },
+  ];
+
+  const newDataCalorias = [
+    { name: 'Proteinas Kcal', value: proteinas * 4 },
+    { name: 'Carboidratos Kcal', value: carboidratos * 4 },
+    { name: 'Gorduras Kcal', value: gorduras * 9 },
   ];
 
   const centerText = `${calorias} kcal`;
 
   return (
-    <PieChart width={360} height={360}>
-      <Pie data={newData} dataKey="value" nameKey="name" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#82ca9d" label paddingAngle={5}>
+    <PieChart width={420} height={360}>
+      <Pie data={newDataCalorias} dataKey="value" cx="50%" cy="50%" outerRadius={60} fill="#8884d8">
+        {newData.map((entry, index) => (
+          <Cell key={`cell-${index}`} fill={COLORS_FADE[index % COLORS_FADE.length]} />
+        ))}
+      </Pie>
+      <Pie data={newData} dataKey="value" nameKey="name" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" label paddingAngle={5}>
         {newData.map((entry, index) => (
           <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
         ))}
       </Pie>
-      <text x="50%" y="48%" textAnchor="middle" dominantBaseline="middle" fontSize="1.2em">
+      <text x="50%" y="43%" textAnchor="middle" dominantBaseline="middle" fontSize="1.2em" style={{ fontWeight: 'bold'}}>
         {centerText}
       </text>
       <Tooltip />
